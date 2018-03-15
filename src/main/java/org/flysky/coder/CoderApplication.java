36b2e8e30480bd9e30750f83b87ab81385f5ee75
hxuhao233 <hxh12342005@qq.com>
2018-03-15 14:09:42 +0800

    test

--- src/main/java/org/flysky/coder/CoderApplication.java
+++ src/main/java/org/flysky/coder/CoderApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class CoderApplication extends SpringBootServletInitializer {
 
-	@Override
 	// to run in outer tomcat
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CoderApplication.class);
 	}
