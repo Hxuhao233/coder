ee2a4918723156f3e58ba6ad2bebb6763b41d0c3
hxuhao233 <hxh12342005@qq.com>
2018-03-27 17:00:02 +0800

    集成redis，增加redis测试用例

---
+++ src/test/java/org/flysky/coder/redisTest.java
@@ -0,0 +1,35 @@
+package org.flysky.coder;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by hxuhao233 on 2018/3/27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class redisTest {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    public void test001() {
+        redisTemplate.opsForValue().set("key1","value1");
+    }
+
+    @Test
+    public void test002() {
+        String value = redisTemplate.opsForValue().get("key1");
+        System.out.println(value);
+    }
+
+}
